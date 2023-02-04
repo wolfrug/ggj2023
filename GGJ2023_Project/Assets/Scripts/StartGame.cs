0c890308ee7c7e489e27b7c769ad9588bf538a7c
pskult <petter.skult@gmail.com>
2023-02-05 00:25:41 +0200

    added main scene + a bunch of UI changes

---
+++ GGJ2023_Project/Assets/Scripts/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour {
+    // Start is called before the first frame update
+    public string startGameScene;
+    public void LoadGameScene () {
+        SceneManager.LoadScene(startGameScene);
+    }
+    public void QuitGame () {
+        Application.Quit ();
+    }
+}
